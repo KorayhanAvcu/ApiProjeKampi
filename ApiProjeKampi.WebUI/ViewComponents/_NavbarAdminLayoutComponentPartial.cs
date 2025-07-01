2d9058ed98d2b84e95a9ec396320ddce6aed3a57
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2025-07-01 23:13:49 +0300

    son ekleme

---
+++ ApiProjeKampi.WebUI/ViewComponents/_NavbarAdminLayoutComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _NavbarAdminLayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
