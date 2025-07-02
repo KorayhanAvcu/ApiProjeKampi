50f9fc8ee2e7df715fe28cc9433fc774f5187b28
KorayhanAvcu <korayhanavcu@gmail.com>
2025-07-02 10:05:20 +0300

    Admin tarafının temeli bitti

--- ApiProjeKampi.WebUI/ViewComponents/_NavbarAdminLayoutComponentPartial.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace ApiProjeKampi.WebUI.ViewComponents
-{
-    public class _NavbarAdminLayoutComponentPartial:ViewComponent
-    {
-        public IViewComponentResult Invoke() { return View(); }
-    }
-}
