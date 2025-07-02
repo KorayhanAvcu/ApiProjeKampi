d28f714355e38be1f0489ee3946e9c98ec56086c
KorayhanAvcu <korayhanavcu@gmail.com>
2025-07-02 08:52:23 +0300

    navbar halledildi

--- ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarNotificationAdminLayoutComponentPartial.cs
+++ ApiProjeKampi.WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarNotificationAdminLayoutComponentPartial.cs
@@ -1,14 +1,14 @@
-﻿using ApiProjeKampi.WebUI.Dtos.ChefDtos;
-using ApiProjeKampi.WebUI.Dtos.MessageDtos;
+﻿using ApiProjeKampi.WebUI.Dtos.MessageDtos;
+using ApiProjeKampi.WebUI.Dtos.NotificationDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
 {
-    public class _NavbarMessageListAdminLayoutComponentPartial : ViewComponent
+    public class _NavbarNotificationAdminLayoutComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        public _NavbarMessageListAdminLayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        public _NavbarNotificationAdminLayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
@@ -16,11 +16,11 @@ namespace ApiProjeKampi.WebUI.ViewComponents.AdminLayoutNavbarViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7000/api/Messages/MessageListByIsReadFalse");
+            var responseMessage = await client.GetAsync("https://localhost:7000/api/Notifications");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultMessageByIsReadFalseDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultNotificationDto>>(jsonData);
                 return View(values);
             }
             return View();
